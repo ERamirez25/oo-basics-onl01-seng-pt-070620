e9628d9b7a860238034caa4de8f4087b2c4a7674
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-23 04:42:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,4 @@ end
 attr_reader :brand
 attr_accessor :color, :size, :material, :condition
 
-der cobble()
-self.condition = "new"
-puts "Your shoe is as good as new!"
-end
-end 
-Shoe.new("Nike")
+
