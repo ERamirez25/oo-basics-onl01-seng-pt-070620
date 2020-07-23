8c789e13a308148b2c2dd0ddb2fd31c5de47c9f8
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-23 04:51:30 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,15 @@
+class Shoe
+  def initialize(brand)
+    @brand = brand 
+end 
+attr_reader :brand
+attr_accessor :color, :size, :material, :condition
+
+end 
+ 
+ def cobble
+   self.condition = "new"
+   puts "Your shoe is as good as new!"
+ end 
+ Shoe.new("Nike")
+l
