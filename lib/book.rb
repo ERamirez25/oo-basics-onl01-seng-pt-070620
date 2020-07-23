26636d6a7a461d7df5bda1bd06b6a49c9ae59802
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-23 04:33:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,34 +2,6 @@ class Book
 end 
 
 
-class Book
-  def initialize(title)
-    @title = title 
-  end 
-end 
 
 
-
-  def title
-    @title 
-  end 
-
-
-def author=(author)
-  @author = author
-end 
-
-
-def author
-  @author
-end
-end 
-
-def page_count=(num)
-  @page_count = num
-end 
-
- def page_count
-   @page_count
- end 
  
